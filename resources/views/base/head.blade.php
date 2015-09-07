f9582093cd5718467b49d1e3f67426068610e81d
hheedat <yuxiangzhao@outlook.com>
2015-09-07 20:50:30 +0800

    change something

--- resources/views/base/head.blade.php
+++ resources/views/base/head.blade.php
@@ -4,7 +4,7 @@
             <a href="/">{{ $user->name }}</a>
             <a class="logout-btn" href="/auth/logout">退出</a>
         @else
-            <a href="/">首页-请登录或注册List</a>
+            <a href="/">首页-登录或注册List</a>
         @endif
         <a class="site-name" href="/">
             <span id="last-update"></span>
